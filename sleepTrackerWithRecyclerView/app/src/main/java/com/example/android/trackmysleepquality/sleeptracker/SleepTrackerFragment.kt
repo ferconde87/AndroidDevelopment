1d3364da4de4f224ed1372bd6420ddd7d1842c05
fer <ferconde87@gmail.com>
2020-06-25 21:36:21 +0700

    ListAdapter

--- sleepTrackerWithRecyclerView/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ sleepTrackerWithRecyclerView/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -105,7 +105,7 @@ class SleepTrackerFragment : Fragment() {
 
         sleepTrackerViewModel.nights.observe(this.viewLifecycleOwner, Observer {
             it?.let {
-                adapter.data = it
+                adapter.submitList(it)
             }
         })
 
